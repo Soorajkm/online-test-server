0db6d68a820f1156c8fb67830a8bfa46b8cf46b6
Soorajkm <surajkumarm283@gmail.com>
2023-12-16 18:12:18 +0530

    some changes

--- app.js
+++ app.js
@@ -27,11 +27,15 @@ app.use("/users", usersRouter);
 app.use("/test", testRouter);
 app.use("/auth", authRouter);
 app.use((req, res, next) => {
+  res.setHaeader(Access - Control - Allow - Origin, "http://localhost:3000");
   res.setHeader(
-    "Access-Control-Allow-Methods",
+    Access - Control - Allow - Methods,
     "GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    Access - Control - Allow - Headers,
+    "Content-Type, Authorization"
+  );
   next();
 });
 app.use(cors());
