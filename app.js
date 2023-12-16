6deb2ca157f23e565e0d5c50f52158ff6be184c4
Soorajkm <surajkumarm283@gmail.com>
2023-12-16 17:53:14 +0530

    some changes

--- app.js
+++ app.js
@@ -26,11 +26,6 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/test", testRouter);
 app.use("/auth", authRouter);
-app.use(cors());
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -38,6 +33,11 @@ app.use((req, res, next) => {
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
+});
+app.use(cors());
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
